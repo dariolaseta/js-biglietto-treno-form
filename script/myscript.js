13324a7d9cb9ac98953807c42a492f979e70602a
dariolaseta <dario.laseta@gmail.com>
2023-05-15 15:14:24 +0200

    created form section

--- script/myscript.js
+++ script/myscript.js
@@ -1,12 +1,15 @@
+const calculate = document.querySelector("p.calculate");
+
 calculate.addEventListener("click", function(){
     const km = document.getElementById('distance').value;
     const userAge = document.getElementById('user-age').value;
     let price = km * 0.233;
     let discountPrice;
 
-    const button = document.querySelector("button.calculate");
-
-
+    let writeKm;
+    let writeUserAge;
+    let writeFinalPrice;
+    
     if(userAge < 18){
         discountPrice = (price / 100) * 19.4;
         price = price - discountPrice;
@@ -19,6 +22,12 @@ calculate.addEventListener("click", function(){
         price = km * 0.233
         console.log("Il prezzo è " + price.toFixed(2));
     }
+
+    writeKm = document.getElementById("final-km").innerHTML = "I chilometri che vuoi percorrere sono: " + km;
+
+    writeUserAge = document.getElementById("final-user-age").innerHTML = "La tua età è: " + userAge;
+
+    writeFinalPrice = document.getElementById("final-price").innerHTML = "Il prezzo del biglietto è: " + price.toFixed(2);
 }
 );
 
